1a0c57d670a85eb59bbecbda25b6921580b7bb39
yl74053 <q874982346@gmail.com>
2019-11-27 14:56:58 -0500

    Design Update

--- js/main.js
+++ js/main.js
@@ -8,15 +8,13 @@ var time = ["0:00", "1:00", "2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:0
 
 var timeN = ["000", "100", "200", "300", "400", "500", "600", "700", "800", "900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2200", "2300", "2400"]
 
-var company = ["All", "United Airlines", "Delta Airlines"]
-
-var companyIATA = ["UA", "DL"]
-
 var city = ["N/A", "Atlanta", "Miami"]
 
-var map = d3.select('#map')
-                .append("g")
-                .attr("transform", "translate( -110 , 0 )")
+var mapholder = d3.select('#map')
+
+var map = mapholder.append("g")
+            .attr("transform", "translate( -110 , 0 )")
+
 
 var info = d3.select('#info')
                 .append("g")
@@ -43,12 +41,12 @@ panel1.append("text")
 panel1.append("text")
         .attr("y", 150)
         .attr("font-size", "18px")
-        .text("Airline Performance")
+        .text("Top Delay Routes")
 
 panel1.append("text")
         .attr("y", 350)
         .attr("font-size", "18px")
-        .text("Top Delay Flights")
+        .text("Airline Performance")
 
 
 var panel2 = info.append("g")
@@ -79,12 +77,12 @@ panel2.append("text")
 panel2.append("text")
         .attr("y", 160)
         .attr("font-size", "18px")
-        .text("Airline Performance")
+        .text("Top Delay Destination")
 
 panel2.append("text")
         .attr("y", 350)
         .attr("font-size", "18px")
-        .text("Top Delay Flights")
+        .text("Airline Performance")
 
 panel2.attr("opacity", 0)
 
@@ -116,12 +114,12 @@ panel3.append("text")
 panel3.append("text")
         .attr("y", 160)
         .attr("font-size", "18px")
-        .text("Airline Performance")
+        .text("Top Delay Origin")
 
 panel3.append("text")
         .attr("y", 350)
         .attr("font-size", "18px")
-        .text("Top Delay Flights")
+        .text("Airline Performance")
 
 panel3.attr("opacity", 0)
 
@@ -154,7 +152,7 @@ panel4.append("text")
 panel4.append("text")
         .attr("y", 350)
         .attr("font-size", "18px")
-        .text("Predicted Delay Time")
+        .text("Predict Delay Time")
 
 panel4.attr("opacity", 0)
 
@@ -166,7 +164,9 @@ var mSelector = d3.select('#month')
                         var v = d3.select(this).node().value
                         if (month.indexOf(v) > 0) {
                             d3.select('#date').property("disabled", false)
+                            d3.select('#date2').property("disabled", false)
                             d3.select('#date').selectAll("option").remove()
+                            d3.select('#date2').selectAll("option").remove()
                             var daydata = day
                             if ( v == "April" || v ==  "June" || v == "September" || v == "November") {
                                 daydata = daydata.slice(0, 31)
@@ -180,8 +180,16 @@ var mSelector = d3.select('#month')
                                 .enter()
                                 .append("option")
                                 .text(function(d){ return d;});
+
+                            d3.select('#date2').selectAll("option")
+                                .data(daydata)
+                                .enter()
+                                .append("option")
+                                .text(function(d){ return d;});
+
                         } else {
                             d3.select('#date').property("disabled", true)
+                            d3.select('#date2').property("disabled", true)
                         }
 
 
@@ -201,6 +209,14 @@ var dOption = dSelector.selectAll("option")
         			.enter()
         			.append("option")
             		.text(function(d){ return d;});
+
+var d2Selector = d3.select('#date2')
+
+var d2Option = d2Selector.selectAll("option")
+                    .data(day)
+                    .enter()
+                    .append("option")
+                    .text(function(d){ return d;});
 
 
 var t1Selector = d3.select('#take-off1')
@@ -239,15 +255,6 @@ var a2Option = a2Selector.selectAll("option")
                     .property("selected", true);
 
 
-var cSelector = d3.select('#company')
-
-var cOption = cSelector.selectAll("option")
-        			.data(company)
-        			.enter()
-        			.append("option")
-            		.text(function(d){ return d;});
-
-
 var tSelector = d3.select('#departure')
                     .on("change", 
 
@@ -268,7 +275,7 @@ var tSelector = d3.select('#departure')
                                 panel3.attr("opacity", 0)
                                 panel4.attr("opacity", 0)
 
-                            } else if ( target == null ) {
+                            } else if ( target == null || origin == target) {
 
                                 panel1.attr("opacity", 0)
                                 panel2.attr("opacity", 1)
@@ -294,7 +301,6 @@ var tSelector = d3.select('#departure')
 
                             }
 
-                            console.log(origin)
                         }
                     )
 
@@ -324,7 +330,7 @@ var aSelector = d3.select('#arrival')
                                 panel3.attr("opacity", 0)
                                 panel4.attr("opacity", 0)
 
-                            } else if ( target == null ) {
+                            } else if ( target == null || origin == target) {
 
                                 panel1.attr("opacity", 0)
                                 panel2.attr("opacity", 1)
@@ -350,8 +356,8 @@ var aSelector = d3.select('#arrival')
 
                             }
 
-                            console.log(target)
                         }
+    
                     )
 
 var aOption = aSelector.selectAll("option")
@@ -378,6 +384,8 @@ var usmap = d3.json("data/states-10m.json").then(
 
     function(us) {
 
+        console.log(us)
+
         map.attr("class", "states")
                 .selectAll("path")
                 .data(topojson.feature(us, us.objects.states).features)
@@ -389,6 +397,146 @@ var usmap = d3.json("data/states-10m.json").then(
                 .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
                 .attr("class", "state-borders")
                 .attr("d", path);
+
+        var legend = mapholder.append("g")
+                        .attr("transform", "translate( 10 , 500 )")
+
+        legend.append("text")
+                .text("On-Time Performance")
+
+        legend.append("rect")
+                .attr("y", 15)
+                .attr("height", 10)
+                .attr("width", 30)
+                .style("fill", "green")
+
+        legend.append("text")
+                .attr("y", 25)
+                .attr("x", 40)
+                .text("Smooth")
+
+        legend.append("rect")
+                .attr("y", 40)
+                .attr("height", 10)
+                .attr("width", 30)
+                .style("fill", "orange")
+
+        legend.append("text")
+                .attr("y", 50)
+                .attr("x", 40)
+                .text("Moderate")
+
+        legend.append("rect")
+                .attr("y", 65)
+                .attr("height", 10)
+                .attr("width", 30)
+                .style("fill", "red")
+
+        legend.append("text")
+                .attr("y", 75)
+                .attr("x", 40)
+                .text("Heavy")
+
+        legend.append("text")
+                .attr("x", 180)
+                .text("Flight Volume")
+
+
+        legend.append("rect")
+                .attr("x", 180)
+                .attr("y", 20)
+                .attr("height", 1)
+                .attr("width", 30)
+                .style("fill", "grey")
+
+        legend.append("text")
+                .attr("y", 25)
+                .attr("x", 220)
+                .text("10")
+
+        legend.append("rect")
+                .attr("x", 180)
+                .attr("y", 40)
+                .attr("height", 2)
+                .attr("width", 30)
+                .style("fill", "grey")
+
+        legend.append("text")
+                .attr("y", 47.5)
+                .attr("x", 220)
+                .text("20")
+
+        legend.append("rect")
+                .attr("x", 180)
+                .attr("y", 60)
+                .attr("height", 5)
+                .attr("width", 30)
+                .style("fill", "grey")
+
+        legend.append("text")
+                .attr("y", 68)
+                .attr("x", 220)
+                .text("50")
+
+        legend.append("rect")
+                .attr("x", 180)
+                .attr("y", 80)
+                .attr("height", 10)
+                .attr("width", 30)
+                .style("fill", "grey")
+
+        legend.append("text")
+                .attr("y", 90)
+                .attr("x", 220)
+                .text("100")
+
+        legend.append("text")
+                .attr("x", 360)
+                .text("Airport Status")
+
+        var pieholder = legend.append("g")
+                            .attr("transform", "translate( 420 , 50 )")
+        
+
+        var piedata = {"Delay": 3, "On-Time": 7}
+
+        var piecolor = d3.scaleOrdinal().domain(piedata).range(["#e74c3c", "#2ecc71"])
+        
+        var pie = d3.pie().value( d => d.value)
+
+        var data_ready = pie(d3.entries(piedata))
+
+        var arc = d3.arc().innerRadius(15).outerRadius(30)
+
+        pieholder.selectAll('piechart')
+                .data(data_ready)
+                .enter()
+                .append('path')
+                    .attr('d', arc)
+                    .attr('fill', d => piecolor(d.data.key))
+        
+
+        pieholder.selectAll('pielabel')
+                .data(data_ready)
+                .enter()
+                .append('text')
+                .text( d => d.data.key)
+                .attr('transform',
+                    function(d){
+                        var pos = arc.centroid(d)
+                        pos[0] = (pos[0]+1) * 1.7
+                        pos[1] = (pos[1]+1) * 1.7
+                        return 'translate('+ pos +')' 
+                    }
+                )
+                .style("text-anchor",
+                    function(d) {
+
+                        var midangle = d.startAngle + (d.endAngle - d.startAngle) /2
+                        return (midangle < Math.PI ? 'start' : 'end')
+                    }
+                )
+        
     }
 )
 
